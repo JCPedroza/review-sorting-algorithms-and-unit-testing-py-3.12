c53c3d108ae3553ad3b7663d4cdafaaa325e32ff
JCPedroza <nemesis4go10@hotmail.com>
2024-04-15 15:33:56 -0600

    refa: add type hintsls

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,8 +1,7 @@
-def bubble_sort(nums):
+def bubble_sort(nums: list[int]) -> list[int]:
     swapped = False
 
     for loop in range(len(nums) - 1):
-        print(loop)
         for idx in range(len(nums) - loop - 1):
             if nums[idx] > nums[idx + 1]:
                 nums[idx], nums[idx + 1] = nums[idx + 1], nums[idx]
@@ -10,5 +9,5 @@ def bubble_sort(nums):
 
         if not swapped:
             return nums
-    
+
     return nums
