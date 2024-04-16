afadcf3d28b6648c8ce59a0b2cf0bfe1a975afa1
JCPedroza <nemesis4go10@hotmail.com>
2024-04-15 22:29:44 -0600

    fix: remove early termination flag

--- algo_selection_sort.py
+++ algo_selection_sort.py
@@ -1,11 +1,12 @@
 """
-This implementation of Selection Sort uses a boolean flag to improve the best case time
-complexity from O(n^2)to O(n) by terminating early if the list is already sorted.
+This implementation of Selection Sort avoids processing the sorted portion of the list
+by having loop + 1 as the start of the inner loop range.
 
-It also avoids processing the sorted portion of the list by having
-loop + 1 as the start of the inner loop.
+No flag is used for early termination optimization since it only works for arrays that
+are already initially sorted, and not for arrays that are sorted during the process, so
+I don't think the optimization is worth the overhead.
 
-Time Complexity: Best O(n) | Avg O(n^2) | Worst O(n^2)
+Time Complexity: Best O(n^2) | Avg O(n^2) | Worst O(n^2)
 Space Complexity: Total O(n) | Aux O(1)
 
 https://en.wikipedia.org/wiki/selection_sort
@@ -19,17 +20,12 @@ from sort_types import IntList, SortingAlgorithm
 def selection_sort(nums: IntList) -> IntList:
     """Sorts the given integer list in ascending order."""
     for loop in range(len(nums)):
-        is_ordered = True
         min_val_idx = loop
 
-        # Loop through the unsorted portion of the list and find the min value
+        # Loop through the unsorted portion and find the min value
         for idx in range(loop + 1, len(nums)):
             if nums[idx] < nums[min_val_idx]:
                 min_val_idx = idx
-                is_ordered = False  # Don't terminate early
-
-        if is_ordered:
-            return nums  # Terminate early
 
         # Move min value of unsorted portion to the new end of the sorted portion
         nums[loop], nums[min_val_idx] = nums[min_val_idx], nums[loop]
