ab02fc8552aecbe96f9ec2161e4dc35b4acaecbd
JCPedroza <nemesis4go10@hotmail.com>
2024-04-15 23:00:28 -0600

    docs: add module docstring

--- algo_insertion_sort.py
+++ algo_insertion_sort.py
@@ -1,3 +1,17 @@
+"""
+This implementation of Selection Sort:
+  * Has built-in early termination optimization.
+  * Could be optimized by reducing the number of swaps and comparisons, but this
+    version is used to preserve logic simplicity.
+
+Time Complexity: Best O(n) | Avg O(n^2) | Worst O(n^2)
+Space Complexity: Total O(n) | Aux O(1)
+
+https://en.wikipedia.org/wiki/insertion_sort
+https://www.programiz.com/dsa/insertion-sort
+https://rosettacode.org/wiki/Sorting_algorithms/Insertion_sort
+"""
+
 from sort_types import IntList, SortingAlgorithm
 
 
@@ -6,6 +20,7 @@ def insertion_sort(nums: IntList) -> IntList:
     for loop in range(1, len(nums)):
         idx = loop
 
+        # Swap left until the correct place inside the sorted portion is found
         while nums[idx - 1] > nums[idx] and idx > 0:
             nums[idx - 1], nums[idx] = nums[idx], nums[idx - 1]
             idx -= 1
