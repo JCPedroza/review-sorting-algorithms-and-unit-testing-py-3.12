d3baeac12d9b1c57395c478e26e93356274e2098
JCPedroza <nemesis4go10@hotmail.com>
2024-04-15 20:14:06 -0600

    file: use the algo_ prefix for algorithm file names

--- sort_repo.py
+++ sort_repo.py
@@ -1,5 +1,5 @@
-from bubble_sort import algorithm as bubble
-from selection_sort import algorithm as selection
-from insertion_sort import algorithm as insertion
+from algo_bubble_sort import algorithm as bubble
+from algo_selection_sort import algorithm as selection
+from algo_insertion_sort import algorithm as insertion
 
 algorithms = [bubble, selection, insertion]
