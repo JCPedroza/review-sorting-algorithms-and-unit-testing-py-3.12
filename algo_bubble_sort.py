c966f6887f5e94534bb06979d9f5007690d469d3
JCPedroza <nemesis4go10@hotmail.com>
2024-04-15 21:55:26 -0600

    doc: add module docstring

--- algo_bubble_sort.py
+++ algo_bubble_sort.py
@@ -1,3 +1,18 @@
+"""
+This implementation of Bubble Sort uses a boolean flag to improve the best case time
+complexity from O(n^2)to O(n) by terminating early if the list is already sorted.
+
+It also avoids processing the sorted portion of the list by having
+len(nums) - loop - 1 as the stop of the inner loop.
+
+Time Complexity: Best O(n) | Avg O(n^2) | Worst O(n^2)
+Space Complexity: Total O(n) | Aux O(1)
+
+https://en.wikipedia.org/wiki/bubble_sort
+https://www.programiz.com/dsa/bubble-sort
+https://rosettacode.org/wiki/Sorting_algorithms/Bubble_sort
+"""
+
 from sort_types import IntList, SortingAlgorithm
 
 
@@ -9,10 +24,10 @@ def bubble_sort(nums: IntList) -> IntList:
         for idx in range(len(nums) - loop - 1):
             if nums[idx] > nums[idx + 1]:
                 nums[idx], nums[idx + 1] = nums[idx + 1], nums[idx]
-                is_sorted = False
+                is_sorted = False  # Don't terminate early
 
         if is_sorted:
-            return nums
+            return nums  # Terminate early
 
     return nums
 
