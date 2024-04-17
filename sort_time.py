07a189b1c9d8174f050b4d7afbde266ea84a164e
JCPedroza <nemesis4go10@hotmail.com>
2024-04-17 17:58:06 -0600

    refa: add type hints

--- sort_time.py
+++ sort_time.py
@@ -1,7 +1,6 @@
 from random import shuffle
 from timeit import default_timer as timer
-from sort_types import Seconds, IntList, Algorithm, AlgorithmList
-from sort_repo import algorithms
+from sort_types import Algorithm, AlgorithmList, IntList, Seconds
 
 
 def time(algo: Algorithm, arg: IntList) -> Seconds:
@@ -13,22 +12,23 @@ def time(algo: Algorithm, arg: IntList) -> Seconds:
     return stop - start
 
 
-def time_many(algos: AlgorithmList, arg: IntList):
+def time_many(algos: AlgorithmList, arg: IntList) -> dict[Algorithm, Seconds]:
     """Measures the running time of the given sorting algorithms, in seconds."""
     shuffle(algos)
     return {algo: time(algo, arg) for algo in algos}
 
 
-def profile(algos: AlgorithmList, arg: IntList, reps: int):
-    totals = {algo: 0 for algo in algos}
+def profile(algos: AlgorithmList, arg: IntList, rep: int) -> list[tuple[str, Seconds]]:
+    """Accumulates and sorts repeated running time measurements."""
+    totals = {algo: 0.0 for algo in algos}
 
-    for _ in range(reps):
+    for _ in range(rep):
         partials = time_many(algos, arg)
         for algo, secs in partials.items():
             totals[algo] += secs
 
     # Sort results in ascending order
     results = [(algo.name, total) for algo, total in totals.items()]
-    results.sort(key = lambda tup: tup[1])
+    results.sort(key=lambda tup: tup[1])
 
     return results
