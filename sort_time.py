88ab4ba74210ec6813d7df054af83e74350e574d
JCPedroza <nemesis4go10@hotmail.com>
2024-04-17 19:58:20 -0600

    fix: process list copies

--- sort_time.py
+++ sort_time.py
@@ -5,8 +5,9 @@ from sort_types import Algorithm, AlgorithmList, IntList, Seconds
 
 def time(algo: Algorithm, arg: IntList) -> Seconds:
     """Measures the running time of the given sorting algorithm, in seconds."""
+    arg_copy = arg[:]
     start = timer()
-    algo.sort(arg)
+    algo.sort(arg_copy)
     stop = timer()
 
     return stop - start
@@ -15,7 +16,7 @@ def time(algo: Algorithm, arg: IntList) -> Seconds:
 def time_many(algos: AlgorithmList, arg: IntList) -> dict[Algorithm, Seconds]:
     """Measures the running time of the given sorting algorithms, in seconds."""
     shuffle(algos)
-    return {algo: time(algo, arg) for algo in algos}
+    return {algo: time(algo, arg[:]) for algo in algos}
 
 
 def profile(algos: AlgorithmList, arg: IntList, rep: int) -> list[tuple[str, Seconds]]:
@@ -23,7 +24,7 @@ def profile(algos: AlgorithmList, arg: IntList, rep: int) -> list[tuple[str, Sec
     totals = {algo: 0.0 for algo in algos}
 
     for _ in range(rep):
-        partials = time_many(algos, arg)
+        partials = time_many(algos, arg[:])
         for algo, secs in partials.items():
             totals[algo] += secs
 
